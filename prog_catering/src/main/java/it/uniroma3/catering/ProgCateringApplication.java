7f7b8f725dd7e954cba2fa298ae2702f19cdc46a
simone_albero <simone.albero29@gmail.com>
2022-06-05 18:21:30 +0200

    Update Grafico Pagina Index
    nota: manca ancora qualcosa

--- prog_catering/src/main/java/it/uniroma3/catering/ProgCateringApplication.java
+++ prog_catering/src/main/java/it/uniroma3/catering/ProgCateringApplication.java
@@ -2,7 +2,6 @@ package it.uniroma3.catering;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @SpringBootApplication
 public class ProgCateringApplication {
